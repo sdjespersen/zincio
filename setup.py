e68080ee41fb5b3acdaea14fe0ae3c2b5b8c7a35
Scott Jespersen <sjespers@stanford.edu>
2020-05-25 00:53:35 -0700

    fix documentation linking for pypi

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup  # type: ignore
+import setuptools  # type: ignore
 
-setup(
-    name='pyzinc',
-    url='https://github.com/sdjespersen/pyzinc',
-    description='Parsing/dumping for Project Haystack Zinc file format',
-    version='0.0.1',
-    author='Scott Jespersen',
-    license='MIT',
-    packages=[
-        'pyzinc',
-    ],
-    requires=[
-        'numpy',
-        'pandas',
-    ],
-)
+setuptools.setup()
